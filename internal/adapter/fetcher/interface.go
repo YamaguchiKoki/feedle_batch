f3f9172793bcf650436130fd2b0f1356e8391410
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-08-07 23:50:04 +0900

    fix: any削除

--- internal/adapter/fetcher/interface.go
+++ internal/adapter/fetcher/interface.go
@@ -6,7 +6,7 @@ import (
 	"github.com/YamaguchiKoki/feedle_batch/internal/domain/model"
 )
 
-type Fetcher[T any] interface {
+type Fetcher[T model.FetchConfigDetail] interface {
 	Name() string
 	Fetch(ctx context.Context, config T) ([]*model.FetchedData, error)
 }
